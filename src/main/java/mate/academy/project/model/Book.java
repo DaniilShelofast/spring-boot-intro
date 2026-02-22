f305b70b5453672a6337e2252f9a20cb32d9e64e
Danyil <shelofast.daniil@gmail.com>
2026-02-22 22:38:09 +0200

    added directory test/resources and update file pom.xml

--- src/main/java/mate/academy/project/model/Book.java
+++ src/main/java/mate/academy/project/model/Book.java
@@ -7,10 +7,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
