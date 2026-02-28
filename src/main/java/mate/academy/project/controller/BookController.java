2f6c51a13777cf8f7bbc47eb3c5da4f4e696bd71
Danyil <shelofast.daniil@gmail.com>
2026-02-28 12:35:59 +0200

    refactor: add status in BookController

--- src/main/java/mate/academy/project/controller/BookController.java
+++ src/main/java/mate/academy/project/controller/BookController.java
@@ -5,16 +5,18 @@ import lombok.RequiredArgsConstructor;
 import mate.academy.project.dto.BookDto;
 import mate.academy.project.dto.CreateBookRequestDto;
 import mate.academy.project.service.BookService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/books")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
@@ -23,12 +25,14 @@ public class BookController {
         return bookService.findAll();
     }
 
-    @GetMapping(value = {"/{id}"})
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
